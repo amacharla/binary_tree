6245a13717c17ba1b4cc1fc39833944a1e6f5de3
Anoop M <noop2016@gmail.com>
2017-10-26 22:18:12 +0000

    added edge cases and 10 done

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -4,7 +4,7 @@ size_t binary_tree_depth(const binary_tree_t *node)
 {
 	size_t count = 0;
 
-	for (; node; node = node->parent)
+	for (;node != NULL && node->parent != NULL; node = node->parent)
 		count++;
-	return count - 1;
+	return count;
 }
