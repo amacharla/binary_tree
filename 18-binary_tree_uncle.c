55e67ad7f43e086cb90c01b999311e8da374c71e
thomaspwang <thomaspatrickwang@gmail.com>
2017-10-27 04:56:37 +0000

    fix betty

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,7 +10,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node == NULL ||
 	    node->parent == NULL ||
 	    node->parent->parent == NULL ||
-            node->parent->parent->left == NULL ||
+	    node->parent->parent->left == NULL ||
 	    node->parent->parent->right == NULL)
 		return (NULL);
 	/* return pointer to unclde node */
