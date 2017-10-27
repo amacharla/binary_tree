24012b2779e77fabf71606986c9b0b70fda608b0
Anoop M <noop2016@gmail.com>
2017-10-27 01:37:02 +0000

    testing 9

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -3,7 +3,7 @@
 size_t binary_tree_depth(const binary_tree_t *node)
 {
 	size_t count = 0;
-
+	/* user `parent` attr to determin how far from parent you are */
 	for (;node != NULL && node->parent != NULL; node = node->parent)
 		count++;
 	return count;
